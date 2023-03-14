9c28712490b41eb14f8396647cfb0f8e3aad5d52
NataliaRamirez <nataliabelen6374@gmail.com>
2023-03-14 18:18:32 -0300

    handlerPostApa

--- api/src/handlers/apaHandler.js
+++ api/src/handlers/apaHandler.js
@@ -1,31 +1,9 @@
 const apaSchema = require("../models/Apa");
 
 const apaHandler = async (req, res) => {
-  /* const { name, password, email, cbu_cvu, description, location, url } =
-    req.body;
-  if (
-    !name ||
-    !password ||
-    !email ||
-    !cbu_cvu ||
-    !description ||
-    !location ||
-    !url
-  )
-    return res.status(404).send("Please complete the data");*/
-  if (!req.body) {
-    return res.json(error.message);
-  }
-  const apa = apaSchema(req.body);
+  const apa = apaSchema(req.body); // pedimos todas las propiedades del esquema.
   apa
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-};
-
-/*  const apa = apaSchema(req.body);
-  apa
-    .validate()
+    .validate() // validamos que manden las props.
     .then(() => {
       apa
         .save()
@@ -33,13 +11,12 @@ const apaHandler = async (req, res) => {
         .catch((error) => res.json({ message: error }));
     })
     .catch((error) => {
-      const errors = error.errors;
-      const message = `Apa validation failed: ${Object.keys(errors)
-        .map((key) => `${key}: ${errors[key].message}`)
+      const errors = error.errors; // guardo los errores en const.
+      const message = `Por favor completar: ${Object.keys(errors) // guardo el mensaje con error de la propiedad.
+        .map((key) => `${key}`) // mapeo la propiedad.
         .join(", ")}`;
-      res.status(400).json({ message });
+      res.status(400).send(message);
     });
 };
- */
 
 module.exports = apaHandler;
