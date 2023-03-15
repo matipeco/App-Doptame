df948dd263f70e8118acf280db6141d1efa7cc7b
herno12 <hernanfd@hotmail.com>
2023-03-15 10:30:43 -0300

    Todas las rutas Pet en esta branch. Haciendo ruta Post

--- api/src/routes/PetRouter.js
+++ api/src/routes/PetRouter.js
@@ -1,4 +1,3 @@
-
 const petRouter = require('express').Router();
 const Pet = require('../models/Pet');
 const Apa = require('../models/Apa');
@@ -17,24 +16,15 @@ petRouter.get =('/', async (req,res)=>{
 
 
 
-//getPetById
+
+//getPetById (VERSIÓN ANTERIOR, SIN POPULATE)
 petRouter.get=('/:petId', async (req,res)=>{
     const {petId}=req.params;
     try {
-        const pet=await Pet.findById(petId)
+        const petWithApa=await Pet.findById(petId).populate('apa');
 //Si encontró la Pet
-        if (pet) {
-//Bucar a qué APA pertenece
-            const apa= await Apa.findById(pet.apa) //Agregué al Model Pet el atributo"apa", cuyo valor es el id de la APA.
-//Si encontró la APA
-            if (apa) {
-                //Tomar su location y asignarla a la Pet
-                const petWithLocation= getPetWithLocation(pet,apa);
-                res.status(200).json(petWithLocation)
-//Si no encontró la Apa
-            } else {
-                res.status(404).json({error: 'APA no encontrada'})
-            }
+        if (petWithApa) {
+            res.status(200).json(petWithApa);
         //Si no encontró la Pet
         } else{
             res.status(404).json({error: 'Mascota no encontrada'});
@@ -44,6 +34,40 @@ petRouter.get=('/:petId', async (req,res)=>{
     }
 })
 
+
+// //getPetById (VERSIÓN ANTERIOR, SIN POPULATE)
+// petRouter.get=('/:petId', async (req,res)=>{
+//     const {petId}=req.params;
+//     try {
+//         const pet=await Pet.findById(petId)
+// //Si encontró la Pet
+//         if (pet) {
+// //Bucar a qué APA pertenece
+//             const apa= await Apa.findById(pet.apa) //Agregué al Model Pet el atributo"apa", cuyo valor es el id de la APA.
+// //Si encontró la APA
+//             if (apa) {
+//                 //Tomar su location y asignarla a la Pet
+//                 const petWithLocation= getPetWithLocation(pet,apa);
+//                 res.status(200).json(petWithLocation)
+// //Si no encontró la Apa
+//             } else {
+//                 res.status(404).json({error: 'APA no encontrada'})
+//             }
+//         //Si no encontró la Pet
+//         } else{
+//             res.status(404).json({error: 'Mascota no encontrada'});
+//         }
+//     } catch (error) {
+//         res.status(404).json(error)
+//     }
+// })
+
+
+
+
+
+
+module.exports =petRouter
 
 
 //postPet
@@ -64,7 +88,6 @@ petRouter.post('/', async (req,res)=>{
         res.status(400).json(error) 
     }
 })
-
 
 
 module.exports =petRouter
