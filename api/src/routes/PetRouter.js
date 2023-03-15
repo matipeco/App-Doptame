8734bfccd8b7b1476497a5d284e29bbc3dac4913
herno12 <hernanfd@hotmail.com>
2023-03-15 11:39:18 -0300

    este no

--- api/src/routes/PetRouter.js
+++ api/src/routes/PetRouter.js
@@ -1,14 +1,13 @@
 const petRouter = require('express').Router();
-const Pet = require('../models/Pet');
-const Apa = require('../models/Apa');
+const petSchema = require('../models/Pet');
+
 const {getPetWithLocation} = require('../controllers/petController');
 
 //getAllPets
 petRouter.get =('/', async (req,res)=>{
     try {
         const allPets= await Pet.find();
-        res.status(200).json(allPets);
-        
+        res.status(200).json(allPets);   
     } catch (error) {
         res.status(400).json({error: 'No se pudieron obtener todas las mascotas'})
     }
@@ -65,29 +64,47 @@ petRouter.get=('/:petId', async (req,res)=>{
 
 
 
+//postPet FORMA NUEVA
 
-
-module.exports =petRouter
-
-
-//postPet
 petRouter.post('/', async (req,res)=>{
-    const {name,age,size,type,image,apa}=req.body
-    try {
-        if (!name || !age || !size || !type || !image || !apa){
-            res.status(400).json({error:'Falta información. La mascota no puede ser dada de alta en el sistema'})
-        } else {
-            const newPet=await Pet.create(name,age,size,type,image,apa)
-            if (newPet) {
-                res.status(200).json({message:'La mascosta ha sido dada de alta con éxito'})
-            } else {
-                res.status(400).json({error: 'La mascota no ha podido ser dada de alta'})
-            }
-        }
-    } catch (error) {
-        res.status(400).json(error) 
+    if (!req.body){
+        res.status(400).json({error:'Falta información. La mascota no puede ser dada de alta en el sistema'})
+    } else{
+        const newPet= petSchema(req.body);
+        newPet
+            .save()
+            .then((data)=>res.json(data))
+            .catch((error)=>res.json({message: error}));
     }
 })
+
+
+
+
+
+
+
+
+
+// //postPet VERSION VIEJA
+// petRouter.post('/', async (req,res)=>{
+//     const {name,age,size,type,image,apa}=req.body
+//     try {
+//         if (!name || !age || !size || !type || !image || !apa) {
+//             res.status(400).json({error:'Falta información. La mascota no puede ser dada de alta en el sistema'})
+//         } else {
+//             const newPet=await Pet.create(name,age,size,type,image,apa)
+             
+//             if (newPet) {
+//                 res.status(200).json({message:'La mascosta ha sido dada de alta con éxito'})
+//             } else {
+//                 res.status(400).json({error: 'La mascota no ha podido ser dada de alta'})
+//             }
+//         }
+//     } catch (error) {
+//         res.status(400).json(error) 
+//     }
+// })
 
 
 module.exports =petRouter
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,5 +4,6 @@ const apaRouter = require("./apaRouter");
 const mainRouter = Router();
 
 mainRouter.use("/apa", apaRouter);
+mainRouter.use("/pets", apaRouter);
 
 module.exports = mainRouter;
