9b906d0bb0c652a447f7985329da5e109159a883
FerRz94 <fernando_88_94@hotmail.com>
2023-03-16 15:57:03 -0300

    rutas de user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const apaRouter = require("./apaRouter");
 const userRouter = require("./userRouter");
+const petRouter = require("./petRouter");
 
 const mainRouter = Router();
 
 mainRouter.use("/apa", apaRouter);
 mainRouter.use("/users", userRouter);
+mainRouter.use("/pets", petRouter);
 
 module.exports = mainRouter;
