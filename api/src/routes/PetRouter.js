1d5d33d422c5416312139f531d84d20a5d37792f
herno12 <hernanfd@hotmail.com>
2023-03-16 11:13:36 -0300

    Rutas Pet Funcionando. Pendiente de armar Controllers

--- api/src/routes/PetRouter.js
+++ api/src/routes/PetRouter.js
@@ -3,24 +3,8 @@ const petSchema = require('../models/Pet');
 const Pet= require('../models/Pet');
 // const {getPetWithLocation} = require('../controllers/petController');
 
-
-
-// getAllPets
-// petRouter.get =('/', async (req,res)=>{
-//     try {
-//         Pet.find({},(error,data)=>{
-//             if (error) console.log(error)
-//             if(data) res.status(200).json(data)
-//         })
-//     } catch (error) {
-//         res.status(400).json(error)
-//     }
-// })
-
-
 // getAllPets
 petRouter.get("/", async (req,res)=>{
-    console.log('Hola Mundo!')
     try {
         const allPets= await Pet.find({});
         res.status(200).json(allPets);   
@@ -31,20 +15,7 @@ petRouter.get("/", async (req,res)=>{
 
 
 
-//getAllPets VIEJA
-// petRouter.get =('/', async (req,res)=>{
-//     try {
-//         const allPets= await Pet.find();
-//         res.status(200).json(allPets);   
-//     } catch (error) {
-//         res.status(400).json({error: 'No se pudieron obtener todas las mascotas'})
-//     }
-// })
-
-
-
-
-//getPetById (VERSIÓN ANTERIOR, SIN POPULATE)
+//getPetById
 petRouter.get('/:petId', async (req,res)=>{
     try {
         const petWithApa=await Pet.findById(req.params.petId).populate('apa');
@@ -53,16 +24,16 @@ petRouter.get('/:petId', async (req,res)=>{
             res.status(200).json(petWithApa);
         //Si no encontró la Pet
         } 
-        // else{
-        //     res.status(404).json({error: 'Mascota no encontrada'});
-        // }
+        else{
+            res.status(404).json({error: 'Mascota no encontrada'});
+        }
     } catch (error) {
         res.status(404).json(error)
     }
 })
 
 
-// //getPetById (VERSIÓN ANTERIOR, SIN POPULATE)
+//getPetById (VERSIÓN ANTERIOR, SIN POPULATE)
 // petRouter.get=('/:petId', async (req,res)=>{
 //     const {petId}=req.params;
 //     try {
@@ -90,56 +61,68 @@ petRouter.get('/:petId', async (req,res)=>{
 // })
 
 
-//postPet FORMA NUEVA
+
+
+
+//createPet
 petRouter.post('/create', async (req,res)=>{
-    if (!req.body){
-        res.status(400).json(error.message)
-    } else{
-        const pet= petSchema(req.body);
-        pet
-            .save()
-            .then((data)=>res.json(data))
-            .catch((error)=>res.json(error));
+    const {name,age,size,type,image,apa}=req.body
+    try {
+        if (!name || !age || !size || !type || !image || !apa) {
+            res.status(400).json({error:'Falta información. La mascota no puede ser dada de alta en el sistema.'})
+        } else {
+            const newPet=await Pet.create(req.body)
+            
+            if (newPet) {
+                res.status(200).json({message:'La mascosta ha sido dada de alta con éxito'})
+            } else {
+                res.status(400).json({error: 'La mascota no ha podido ser dada de alta'})
+            }
+        }
+    } catch (error) {
+        res.status(400).json(error) 
     }
 })
 
-
-// //postPet VERSION VIEJA
-// petRouter.post('/', async (req,res)=>{
-//     const {name,age,size,type,image,apa}=req.body
-//     try {
-//         if (!name || !age || !size || !type || !image || !apa) {
-//             res.status(400).json({error:'Falta información. La mascota no puede ser dada de alta en el sistema'})
-//         } else {
-//             const newPet=await Pet.create(name,age,size,type,image,apa)
-             
-//             if (newPet) {
-//                 res.status(200).json({message:'La mascosta ha sido dada de alta con éxito'})
-//             } else {
-//                 res.status(400).json({error: 'La mascota no ha podido ser dada de alta'})
-//             }
-//         }
-//     } catch (error) {
-//         res.status(400).json(error) 
+// //createPet: otra syntax
+// petRouter.post('/create', async (req,res)=>{
+//     if (!req.body){
+//         res.status(400).json(error.message)
+//     } else{
+//         const pet= petSchema(req.body);
+//         pet
+//             .save()
+//             .then((data)=>res.json(data))
+//             .catch((error)=>res.json(error));
 //     }
 // })
 
 
-
-petRouter.put('/:petId', async (req, res) => {
+//editPet
+petRouter.put('/edit/:petId', async (req, res) => {
     try {
         const petEdit = await Pet.findByIdAndUpdate(
             req.params.petId,
             req.body,
             { new: true }
         );
-        console.log(petEdit)
+        // console.log(petEdit)
         res.status(200).json('Mascota modificada exitosamente');
     } catch (error) {
         console.log(error)
         res.status(400).json('La mascota no pudo ser editada');
     }
 });
+
+//deletePet
+petRouter.delete('/delete/:petId', async (req, res) => {
+    try {
+        const pet = await Pet.findByIdAndDelete(req.params.petId);
+        res.status(200).json('La mascosta fue eliminada definitivamente de la base de datos');
+    } catch (error) {
+        res.status(400).json({ error: 'La mascota no fue eliminada de la base de datos' });
+    }
+})
 
 
 module.exports = petRouter;
