dc279b034c3a25fbda2f48b52b06c98a9e16733a
Diego Cusmai <112570306+dcusmai@users.noreply.github.com>
2023-03-16 21:14:08 -0300

    Apa routes, handlers and controllers

--- api/src/handlers/apaHandler.js
+++ api/src/handlers/apaHandler.js
@@ -1,6 +1,69 @@
 const apaSchema = require("../models/Apa");
+const { createApa, putApa, getApaById, getAllApas, getApasByName, deleteApaById } = require('../controllers/apaController');
 
-const apaHandler = async (req, res) => {
+// POST| /create
+const createApaHandler = async (req, res) => {
+  try {
+      const { name, password, email, cbu_cvu, description, location, url } = req.body;  
+      const newApa = await createApa(name, password, email, cbu_cvu, description, location, url);  
+      res.status(200).json(newApa);  
+  } catch (error) {
+      res.status(404).send(error.message);
+  }
+};
+
+// PUT| /edit/:id 
+const putApaHandler = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const { name, password, email, cbu_cvu, description, location, url } = req.body;  
+      const modApa = await putApa(id, name, password, email, cbu_cvu, description, location, url);  // {id, name, password, email, cbu_cvu, description, location, url}
+      res.status(200).json(modApa);  
+  } catch (error) {
+      res.status(404).send(error.message);
+  }
+};
+
+// GET BY ID| /:id
+const getApaByIdHandler = async (req, res) => { 
+  const { id } =  req.params;
+  console.log(req.params);
+    try {
+        const apaById = await getApaById(id);      
+        if(!apaById) throw new Error ('La APA que deseas modificar no existe :(');  
+        res.status(200).json(apaById);  
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+  };
+
+// GET ALL| /
+const getAllApasHandler =  async (req, res) => {
+  const { name }  = req.query;  
+  try {
+      const results = name ? await getApasByName(name) : await getAllApas()
+      if(!results) throw new Error('Apa no encontrada :(');
+      res.status(200).json(results);
+  } catch (error) {
+      res.status(404).send(error.message);
+  };
+};
+
+// DELETE BY ID| /delete/:id
+const deleteApaByIdHandler = async (req, res) => { 
+  const { id } =  req.params;
+    try {
+        const apaById = await deleteApaById(id);      
+        if(!apaById) throw new Error ('La APA que deseas eliminar no existe :(');  
+        res.status(200).json(apaById);  
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+  };
+
+
+// Todo esto desarrollado por Nati
+// const apaHandler = async (req, res) => { // Diego: comentado
   /* const { name, password, email, cbu_cvu, description, location, url } =
     req.body;
   if (
@@ -13,7 +76,8 @@ const apaHandler = async (req, res) => {
     !url
   )
     return res.status(404).send("Please complete the data");*/
-  if (!req.body) {
+  
+  /*  if (!req.body) { // Diego: comentado
     return res.json(error.message);
   }
   const apa = apaSchema(req.body);
@@ -21,7 +85,7 @@ const apaHandler = async (req, res) => {
     .save()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
-};
+}; */
 
 /*  const apa = apaSchema(req.body);
   apa
@@ -42,4 +106,10 @@ const apaHandler = async (req, res) => {
 };
  */
 
-module.exports = apaHandler;
+module.exports = { 
+    createApaHandler,
+    putApaHandler,
+    getApaByIdHandler,
+    getAllApasHandler,
+    deleteApaByIdHandler
+ }; // Diego: Acá puedo hacer destructuring y mandar handler por handler. Antes apaHandler
--- api/src/routes/apaRouter.js
+++ api/src/routes/apaRouter.js
@@ -1,7 +1,25 @@
 const { Router } = require("express");
 const apaRouter = Router();
-const apaHandler = require("../handlers/apaHandler");
+const { 
+        createApaHandler, 
+        putApaHandler, 
+        getApaByIdHandler, 
+        getAllApasHandler, 
+        deleteApaByIdHandler 
+    } = require("../handlers/apaHandler"); // Diego: Traerme todos los handlers con destructuring (antes solo apaHandler)
 
-apaRouter.post("/", apaHandler);
+// apaRouter.post("/", apaHandler); ---> Nati
+
+// Diego: Voy a modularizar todas las rutas para más orden. 
+
+apaRouter.post('/', createApaHandler);
+
+apaRouter.put('/:id', putApaHandler); 
+
+apaRouter.get('/:id', getApaByIdHandler); 
+
+apaRouter.get('/', getAllApasHandler); // Diego: Nuevo
+
+apaRouter.delete('/:id', deleteApaByIdHandler); // Diego: Nuevo
 
 module.exports = apaRouter;
