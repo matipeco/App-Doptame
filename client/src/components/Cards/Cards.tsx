e8d2ab7416764d06a483f8c852c453cf32888ef1
angykizzi <angelyescafuller@gmail.com>
2023-03-17 19:18:44 -0400

    Paginacion

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -2,11 +2,12 @@ import { Card } from '../Card/Card'
 import { useSelector, useDispatch } from 'react-redux'
 import { StateType } from '../../redux/reducer/reducer'
 import { Pet } from '../../redux/types'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getPets } from '../../redux/actions/actions'
 import { AnyAction } from 'redux'
 import { useParams } from 'react-router-dom'
 
+import PaginationControlled from '../Pagination/Pagination'
 
 export const Cards = () => {
   const { allPets } = useSelector((state: StateType) => state)
@@ -15,6 +16,7 @@ export const Cards = () => {
   const filteredPets = allPets.filter((pet: Pet) => pet.type === category)
 
   const dispatch = useDispatch();
+  const [currentPets, setCurrentPets] = useState<Pet[]>([])
 
   useEffect(() => {
     dispatch(getPets() as unknown as AnyAction)
@@ -22,11 +24,13 @@ export const Cards = () => {
 
   // Mostramos las tarjetas filtradas en el componente
   return (
-    <div>
-      {filteredPets.map((pet: Pet) => (
+    <><div>
+      {currentPets.map((pet: Pet) => (
         <Card key={pet._id} pet={pet} />
       ))}
     </div>
+    <PaginationControlled filteredPets={filteredPets} setCurrentPets={setCurrentPets} /></>
+
   )
 }
 
