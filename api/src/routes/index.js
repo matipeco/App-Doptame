cc2210e01391a83c4531e79beb523e4e6e4b7163
herno12 <hernanfd@hotmail.com>
2023-03-21 11:09:39 -0300

    add package.json (instalado bcrypt), + index.js con routes userLogin y apaLogin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const apaRouter = require("./apaRouter");
 const petRouter = require("./petRouter");
 const userRouter = require("./userRouter");
+const userLoginRouter = require("./userLoginRouter");
+const apaLoginRouter = require("./apaLoginRouter");
 
 const mainRouter = Router();
 
 mainRouter.use("/apa", apaRouter);
 mainRouter.use("/pets", petRouter);
 mainRouter.use("/users", userRouter);
+mainRouter.use("/userLogin", userLoginRouter);
+mainRouter.use("/apaLogin", apaLoginRouter);
 
 module.exports = mainRouter;
