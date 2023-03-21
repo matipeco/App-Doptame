3fd52f6d5e9c198aa2e1f6ec7d2a113fb8f22cc3
herno12 <hernanfd@hotmail.com>
2023-03-21 18:26:13 -0300

    Controller login terminado

--- api/src/routes/apaLoginRouter.js
+++ api/src/routes/apaLoginRouter.js
@@ -1,8 +1,6 @@
 const apaLoginRouter = require('express').Router();
 const {apaLogin} = require("../handlers/apaLoginHandler"); 
 
-
-
 apaLoginRouter.post('/', apaLogin)
   
   module.exports = apaLoginRouter;
