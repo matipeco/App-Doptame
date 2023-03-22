c59dcfca6b67bf1653ba7ef9d285474fbf063688
NataliaRamirez <nataliabelen6374@gmail.com>
2023-03-22 01:03:04 -0300

    angyPagination

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -6,33 +6,49 @@ import { useEffect, useState } from 'react'
 import { getPets } from '../../redux/actions/actions'
 import { AnyAction } from 'redux'
 import { useParams } from 'react-router-dom'
-
 import PaginationControlled from '../Pagination/Pagination'
 
 export const Cards = () => {
-  const { allPets } = useSelector((state: StateType) => state)
-  const { category } = useParams()
-  // Filtramos los animales según la categoría seleccionada
-  const filteredPets = allPets.filter((pet: Pet) => pet.type === category)
+  const { allPets } = useSelector((state: StateType) => state);
+  const { category } = useParams();
 
   const dispatch = useDispatch();
-  const [currentPets, setCurrentPets] = useState<Pet[]>([])
+
+  const [currentPage, setCurrentPage] = useState(1); // Agregamos estado para currentPage
+
+  const PageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber); // Actualizamos el estado de currentPage al seleccionar una pÃ¡gina
+  };
 
   useEffect(() => {
     dispatch(getPets() as unknown as AnyAction)
-  }, [])
+  }, [dispatch])
 
-  //Hola este es un comentario que hay que borrar//
 
-  // Mostramos las tarjetas filtradas en el componente
+  const filteredPets = allPets.filter((pet: Pet) => pet.type === category);
+  const itemsPerPage = 5;
+  const totalItems = filteredPets.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = filteredPets.slice(startIndex, endIndex);
+
+  console.log(filteredPets)
   return (
-    <><div>
-      {currentPets.map((pet: Pet) => (
-        <Card key={pet._id} pet={pet} />
-      ))}
-    </div>
-      <PaginationControlled filteredPets={filteredPets} setCurrentPets={setCurrentPets} /></>
 
-  )
-}
+    <>
+      <div>
+        {currentItems.map((pet: Pet) => (
+          <Card key={pet._id} pet={pet} />
+        ))}
+      </div>
+      <PaginationControlled
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        onPageChange={PageChange}
+      />
+    </>
+
+  );
+};
 
