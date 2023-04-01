55cf65db59117514b0b26e5b637c54de745cd6e6
Damian Figueroa <damy.mtl@gmail.com>
2023-03-31 22:22:01 -0300

    botoncito fav y delete funcionando

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -5,60 +5,72 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { postFavorite, deleteFavorite } from '../../redux/actions/actions'
 import { AnyAction } from 'redux';
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+
 
 type Props = {
   pet: Pet
 }
 
 export const Card: FunctionComponent<Props> = ({ pet }) => {
-    const dispatch = useDispatch()
-    const [isFav, setIsFav] = useState(false);
+  const location = useLocation();
+  const dispatch = useDispatch()
+  const [isFav, setIsFav] = useState(() => {
+      const storedValue = localStorage.getItem(`fav-${pet._id}`)
+      return storedValue ? JSON.parse(storedValue) : false
+  });
 
-    const handlerIsFav = async () => {
-      const token = localStorage.getItem("token");
-    
-      if (token) {
-        const payload = token.split(".")[1];
-        const decodedPayload = atob(payload);
-        const user = JSON.parse(decodedPayload);
-        console.log(user.id);
-        console.log(pet)
-        try {
-          if (isFav) {
-            dispatch(deleteFavorite(pet._id ?? '', user.id) as unknown as AnyAction)
-          } else {
-            dispatch(postFavorite(pet._id ?? '', user.id) as unknown as AnyAction)
-          }
-          setIsFav(!isFav); // actualizar el estado después de que la acción se complete
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-
-    
+  const token = localStorage.getItem("token");
+  const user = token ? JSON.parse(atob(token.split(".")[1])) : null;
   
-    
+  useEffect(() => {
+      localStorage.setItem(`fav-${pet._id}`, JSON.stringify(isFav));
+  }, [isFav, pet._id])
 
-    
-  return (
-    <article className={style.card}>
-    
-      <img src={pet.image} alt={pet.name} className={style.img} />
-      <h3>{pet.name}</h3>
-      <h3>{pet.age} Años</h3>
-      <h3>{pet.size}</h3>
-      <div>
-      <Link className={style.link} to={`/detail/${pet._id}`}>
-        Conoceme...
-      </Link>
-      {
-            isFav ? (<button onClick={handlerIsFav} className={style.botonFav}>❤️</button>) 
-            : (<button onClick={handlerIsFav} className={style.botonFav}>🤍</button>)
+  const handlerIsFav = async (userId: string) => {
+      try {
+          if (isFav) {
+              dispatch(deleteFavorite(pet._id ?? '', userId) as unknown as AnyAction)
+          } else {
+              dispatch(postFavorite(pet._id ?? '', userId) as unknown as AnyAction)
+          }
+          setIsFav(!isFav);
+      } catch (error) {
+          console.error(error);
       }
-      </div>
-    
-    </article>
+  };
 
+  // Agrega este código para mostrar el botón X cuando el usuario está en la ruta de favoritos
+  const showXButton = location.pathname.startsWith("/favorites");
+  
+  return (
+      <article className={style.card}>
+          <img src={pet.image} alt={pet.name} className={style.img} />
+          <h3>{pet.name}</h3>
+          <h3>{pet.age} Años</h3>
+          <h3>{pet.size}</h3>
+          <div>
+              <Link className={style.link} to={`/detail/${pet._id}`}>
+                  Conoceme...
+              </Link>
+              {showXButton ? (
+                  <button onClick={() => {
+                      dispatch(deleteFavorite(pet._id ?? '', user.id) as unknown as AnyAction)
+                      window.location.reload(); // recargar la página después de borrar el favorito
+                  }} className={style.botonFav}>
+                      X
+                  </button>
+              ) : (
+                  user ? (
+                      isFav ? (
+                          <button onClick={() => handlerIsFav(user.id)} className={style.botonFav}>❤️</button>
+                      ) : (
+                          <button onClick={() => handlerIsFav(user.id)} className={style.botonFav}>🤍</button>
+                      )
+                  ) : null
+              )}
+          </div>
+      </article>
   )
 }
