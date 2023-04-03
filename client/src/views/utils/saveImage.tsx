5bae20a9a745c1ce1295acf06f6b92280e0919e0
angykizzi <angelyescafuller@gmail.com>
2023-04-03 09:47:34 -0400

    prueba cloudinary

---
+++ client/src/views/utils/saveImage.tsx
@@ -0,0 +1,14 @@
+export const saveImages = async (file: string | Blob) => {
+  let urlImage = "";
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "presetImage");
+  await fetch("https://api.cloudinary.com/v1_1/do1buub4f/image/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((file) => (urlImage = file.secure_url))
+    .catch((e) => console.log(e));
+  return urlImage;
+};
